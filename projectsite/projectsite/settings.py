7924c18a68fab771423603913ee0fb8692a0ae4c
tazz20 <202180392@psu.palawan.edu.ph>
2025-04-03 23:19:05 +0800

    allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure--*bbm$w=pf(s=1&($hh(anu+$_=y)ib9nwwd8_%5mf8ip-(&l=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['PSUsphere.pythonanywhere.com', '127.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
