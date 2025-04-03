580d3cfc7ecacc37f48af2fc683205971f243764
tazz20 <202180392@psu.palawan.edu.ph>
2025-04-03 22:47:43 +0800

    done initial data

--- projectsite/studentorg/management/commands/create_initial_data.py
+++ projectsite/studentorg/management/commands/create_initial_data.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.fake = Faker()
+        self.fake_ph = Faker('en_PH')
         self.create_organizations(10)
         self.create_students(50)
         self.create_membership(10)
@@ -17,31 +18,34 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('No colleges found. Organizations cannot be created.'))
             return
 
-        for _ in range(count):
-            organization_name = ' '.join(self.fake.words(2)).title()
-            Organization.objects.create(
-                name=organization_name,
+        organizations = [
+            Organization(
+                name=' '.join(self.fake.words(2)).title(),
                 college=self.fake.random_element(colleges),
                 description=self.fake.sentence()
             )
+            for _ in range(count)
+        ]
+        Organization.objects.bulk_create(organizations)
         self.stdout.write(self.style.SUCCESS('Initial data for organizations created successfully.'))
 
     def create_students(self, count):
-        fake_ph = Faker('en_PH')
         programs = list(Program.objects.all())
         if not programs:
             self.stdout.write(self.style.WARNING('No programs found. Students cannot be created.'))
             return
 
-        for _ in range(count):
-            student_id = f'{fake_ph.random_int(2020, 2024)}-{fake_ph.random_int(1, 8)}-{fake_ph.random_number(digits=4)}'
-            Student.objects.create(
-                student_id=student_id,
-                lastname=fake_ph.last_name(),
-                firstname=fake_ph.first_name(),
-                middlename=fake_ph.last_name(),
+        students = [
+            Student(
+                student_id=f'{self.fake_ph.random_int(2020, 2024)}-{self.fake_ph.random_int(1, 8)}-{self.fake_ph.random_number(digits=4)}',
+                lastname=self.fake_ph.last_name(),
+                firstname=self.fake_ph.first_name(),
+                middlename=self.fake_ph.last_name(),
                 program=self.fake.random_element(programs)
             )
+            for _ in range(count)
+        ]
+        Student.objects.bulk_create(students)
         self.stdout.write(self.style.SUCCESS('Initial data for students created successfully.'))
 
     def create_membership(self, count):
@@ -52,10 +56,13 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('No students or organizations found. Memberships cannot be created.'))
             return
 
-        for _ in range(count):
-            OrgMember.objects.create(
+        memberships = [
+            OrgMember(
                 student=self.fake.random_element(students),
                 organization=self.fake.random_element(organizations),
                 date_joined=self.fake.date_between(start_date='-2y', end_date='today')
             )
+            for _ in range(count)
+        ]
+        OrgMember.objects.bulk_create(memberships)
         self.stdout.write(self.style.SUCCESS('Initial data for student memberships created successfully.'))
