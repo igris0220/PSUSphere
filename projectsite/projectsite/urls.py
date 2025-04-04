bf16d7a84053a29720ae76a7d645d6876d5db06b
takeshii17 <lancen107@gmail.com>
2025-04-05 01:02:34 +0800

    wow naman

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrgnizationList
+from studentorg.views import HomePageView, OrganizationList
 from studentorg import views
 
 urlpatterns = [
